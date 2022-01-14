531e2b777dfd0858b20f201471605ed62b9e3e8d
Krtz_Dev <krtzgames.mail@gmail.com>
2022-01-14 10:51:26 +0100

    Doc and UI fixes

--- Assets/Scripts/InteractionSystem/IInteractable.cs
+++ Assets/Scripts/InteractionSystem/IInteractable.cs
@@ -1,5 +1,13 @@
 public interface IInteractable<T>
 {
+    /// <summary>
+    /// Implement your specific interaction logic here.
+    /// </summary>
+    /// <param name="instigator">The instigator of the interaction</param>
     void Interact(T instigator);
+    /// <summary>
+    /// Provide the PLayer with some short information about the interaction
+    /// </summary>
+    /// <returns>The returned string will be shown in the ingame UI, when the respective object is looked at.</returns>
     string GetInteractionInfo();
 }
